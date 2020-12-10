5f42e05c406f67686e57ff975260a47efb2c535c
Thomas Whittle <tewhittle@gmail.com>
2020-12-10 12:38:10 -0500

    Add transition js, contact.html

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,19 @@
 const work = document.getElementById("work")
 const contact = document.getElementById("contact")
+const workInfo = document.getElementById("workInfo")
+const contactInfo = document.getElementById("contactInfo")
 
-work.addEventListener("click", () => {
+function animate(el) {
+    transition.begin(el, ["transform translateY(0) translateY(200px) 1s linear",
+    "opacity 1 0 1s linear"  
+    ], {
+        onTransitionEnd: function(el, finished) {
+           window.location.href = "develop/contact.html" 
+        }
+    })
+}
 
+contact.addEventListener("click", () => {
+    contactInfo.appendChild
+    animate(document.body)
 })
